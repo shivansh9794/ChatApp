3b177f9614e417f8d5ea7a53bfd9c5bcb76ad7ab
Sachin Singh <sachinsingh9272@gmail.com>
2025-02-26 10:34:39 +0530

    Created data models

---
+++ backend/model/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    pic:{
+        type:String,
+        required: true,
+        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+    }
+},{timestamps:true})
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
