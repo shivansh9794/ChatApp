74b69dea03bfac486d7244ee1a685d84708b7120
shivansh9794 <mshivansh549@gmail.com>
2025-02-26 13:18:03 +0530

    changes

--- backend/controller/userControllers.js
+++ backend/controller/userControllers.js
@@ -2,15 +2,15 @@ import asyncHandler from 'express-async-handler';
 import User from '../model/userModel.js';
 
 
-export const registerUser = asyncHandler( async () => {
-    const {name, email, password, pic} = req.body
+export const registerUser = asyncHandler(async () => {
+    const { name, email, password, pic } = req.body
 
-    if(!name || !email || !password){
+    if (!name || !email || !password) {
         res.status(400);
         throw new Error("All fields are required");
     }
-    const userExists = await User.findOne({email})
-    if(userExists){
+    const userExists = await User.findOne({ email })
+    if (userExists) {
         res.status(400);
         throw new Error("User already exists");
     }
@@ -21,26 +21,30 @@ export const registerUser = asyncHandler( async () => {
         pic
     });
 
-    if(user){
+    if (user) {
         res.status(201).json({
-            _id:user._id,
+            _id: user._id,
             name: user.name,
-            email:user.email,
-            pic:user.pic,
+            email: user.email,
+            pic: user.pic,
         })
     }
-    else{
+    else {
         throw new Error("Failed to Create the user");
     }
 });
 
 
 // we are going to use queries 
-export const allUsers = asyncHandler(async (req,res)=>{
-    const keyword  = req.query.search ? {
-        $or:[
-            { name:{ $regex: req.query.search, $options:"i" }},
-            {},
+export const allUsers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search ? {
+        $or: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { email : { $regex: req.query.search, $options: "i" } },
         ]
-    }  
+    }
+        : {}
+    const users = await User.find(keyword)
+    // .find({ _id: { $ne: req.user._id } })  // after JWT
+    res.send(users);
 });
