8d2bb66d1d30776f65d4b4c61b13cf67bfcf8e5a
shivansh9794 <mshivansh549@gmail.com>
2025-02-27 11:30:40 +0530

    added Nevigations and login states

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ChatProvider from '../src/context/chatProvider.jsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <chatProvider>
-    <App />
-  </chatProvider>
+  <BrowserRouter>
+    <ChatProvider>
+      <App />
+    </ChatProvider>
+  </BrowserRouter>
 
-
-  // </StrictMode>,
 )
